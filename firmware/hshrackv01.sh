1ce864b945c5fb9824be0369163b080c00f1c5cb
six0four <mdrk0011@humber.ca>
2017-06-16 16:05:21 -0400

    added jpeg to script

--- firmware/hshrackv01.sh
+++ firmware/hshrackv01.sh
@@ -4,9 +4,9 @@ echo 'start update, upgrade, installs (internet needed) script'
 #sudo apt-get upgrade -y
 #sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password '
 #sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password '
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password "
+#sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+#sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+#sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password "
 sudo apt-get install \
 vim vim-gtk \
 automake \
@@ -21,7 +21,7 @@ xscreensaver \
 apache2 \
 mysql-server mysql-client \
 php5 php5-mysql php5-curl phpmyadmin \
-buildessential -y
+jpeg jpeg-dev Libjpeg Libjpeg-dev -y
 #wget http://www.genlogic.com/download/glg-CE-3-6-linux-pi-arm6.tar.gz
 #gunzip /usr/local/glg/glg-3-6-linux-pi-arm6.tar.gz 
 #sudo tar xvf glg-3-6-linux-pi-arm6.tar --directory /usr/local
