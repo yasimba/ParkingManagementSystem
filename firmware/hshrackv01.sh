5b9e1f31b01efdcd40a462238000437cababf72e
six0four <mdrk0011@humber.ca>
2017-06-16 15:29:23 -0400

    added glgtoolkit to script

--- firmware/hshrackv01.sh
+++ firmware/hshrackv01.sh
@@ -2,21 +2,26 @@
 echo 'start update, upgrade, installs (internet needed) script'
 #sudo apt-get update
 #sudo apt-get upgrade -y
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password '
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password '
+#sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password '
+#sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password '
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password "
 sudo apt-get install \
 vim vim-gtk \
 automake \
 codeblocks \
 arduino \
 clamav \
-git-core \
 filezilla \
+git-core \
+gparted \
 xorg \
 xscreensaver \
 apache2 \
 mysql-server mysql-client \
-php5 php5-mysql php5-curl phpmyadmin -y
+php5 php5-mysql php5-curl phpmyadmin \
+glgtoolkit -y
 #wget https://download.zerotier.com/zerotier-one-armhf.deb
 #sudo dpkg --install zerotier-one-armhf.deb
 echo finished script
