1186acab0ef4191c4cf5494a95a6697335604a33
six0four <mdrk0011@humber.ca>
2017-06-16 16:11:51 -0400

    added Xp to script

--- firmware/hshrackv01.sh
+++ firmware/hshrackv01.sh
@@ -23,7 +23,8 @@ mysql-server mysql-client \
 php5 php5-mysql php5-curl phpmyadmin \
 Libjpeg-dev  \
 libjpeg8-dev libtiff5-dev  \
-libx11-dev libxpm-dev -y
+libx11-dev libxpm-dev \
+Xp Xp-dev LibXp-dev -y
 #wget http://www.genlogic.com/download/glg-CE-3-6-linux-pi-arm6.tar.gz
 #gunzip /usr/local/glg/glg-3-6-linux-pi-arm6.tar.gz 
 #sudo tar xvf glg-3-6-linux-pi-arm6.tar --directory /usr/local
