ca88180a3edd87580cf45ef90504ab8c4c139e85
six0four <mdrk0011@humber.ca>
2017-06-16 13:26:19 -0400

    updated script

--- firmware/hshrackv01.sh
+++ firmware/hshrackv01.sh
@@ -3,7 +3,11 @@ echo start update
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install \
-vim vim-gtk
+vim vim-gtk \
+automake \
+codeblocks \
+arduino \
+libreoffice
 wget https://download.zerotier.com/zerotier-one-armhf.deb
 sudo dpkg --install zerotier-one-armhf.deb
 echo finished update
